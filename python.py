fe048738a7fdc6254331f729e12209441d3006ad
Balaji-commits <balanxtgenn@gmail.com>
2025-02-17 20:13:38 +0530

    Update python.py

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ num1 = 1.5
 num2 = 6.3
 
 # Add two numbers
-sum = num1 + num3
-#add the value put it in some container
+sum = num1 + num2
+#add the value and put it in some container
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
